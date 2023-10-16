681e52da6ca5fa2989ae27c43319b6cdc2adb3d6
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-16 16:57:04 +0700

    Initial Commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kilogram/screens/home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController numberController = TextEditingController();
+  bool isNumberValid = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 37),
+              Align(
+                alignment: Alignment.center,
+                child: Image.asset("assets/images/LogoKg.png"),
+              ),
+              SizedBox(height: 55),
+              SafeArea(
+                child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text('Phone Number'),
+                          Text(
+                            "*",
+                            style: TextStyle(color: Colors.red),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      TextField(
+                        controller: numberController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          errorText: isNumberValid
+                              ? null
+                              : "Phone number doesn't valid",
+                          prefixIcon: Icon(Icons.call),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      ),
+                      SizedBox(height: 445),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF1E90FF)),
+                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              isNumberValid = numberController.text.isNotEmpty;
+                              if (isNumberValid) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const Homescreen()),
+                                );
+                              }
+                            });
+                          },
+                          child: Text(
+                            "Continue",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
